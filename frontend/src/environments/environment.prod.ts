244456a4872bd9850aab0f64891771eb7aaba9c0
jerryxguo <jerryxguo@users.noreply.github.com>
2020-04-23 19:05:03 +1000

    add missing files

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,40 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  api: "https://lu63s4ejp0.execute-api.ap-southeast-2.amazonaws.com/api/getdata",
+  genres: [
+    {
+      "name": "state",
+      "field": ""
+    },
+    {
+      "name": "local-government",
+      "field": "lga_name19"
+    },
+    {
+      "name": "postcode",
+      "field": "postcode"
+    }
+  ],
+  resourceId:"21304414-1ff1-4243-a5d2-f52778048b29",
+  limit: 10000,
+  days: 45,
+  token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjI0MzQ2NzAzOTgsImp0aSI6InVzZXIiLCJpc3MiOiJpc3N1ZXIifQ.MSys5QXzjp4_jBEMXC9uxfXAXg2qxg120Q2Zvs4BDCU",
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
---
+++ frontend/src/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+import * as am4core from "@amcharts/amcharts4/core";
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+//am4core.addLicense("CH145052332");
